6158eeb4eb4f46857f7e44cf06e4e7a567f12ac1
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-14 13:44:14 -0200

    Arquivos de refresh e redirect.

---
+++ JS-08-Refresh_ou_Redirect.js
@@ -0,0 +1,8 @@
+// Isso é apenas um comentário:
+// Definindo a varíavel nome
+
+var nome = "Alexandre";
+
+/* Também esse é um outro 
+   comentário que pode ficar 
+   entre asteriscos e barras */
